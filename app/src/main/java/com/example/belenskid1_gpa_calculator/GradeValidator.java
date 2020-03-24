3434b5b6e32cf28c957cccbbfd1e3a31a825b1a6
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-03-24 17:13:18 -0400

    added validator for the grades inputs

---
+++ app/src/main/java/com/example/belenskid1_gpa_calculator/GradeValidator.java
@@ -0,0 +1,21 @@
+package com.example.belenskid1_gpa_calculator;
+
+import android.widget.EditText;
+
+public class GradeValidator {
+    public GradeValidator(){
+
+    }
+    public Boolean Validate(EditText field){
+        String gradeText = field.getText().toString();
+        double grade = Double.parseDouble(gradeText);
+        if (gradeText.length() == 0)
+            return false;
+        else if (grade > 100)
+            return false;
+        else if (grade < 0)
+            return false;
+        else
+            return true;
+    }
+}
