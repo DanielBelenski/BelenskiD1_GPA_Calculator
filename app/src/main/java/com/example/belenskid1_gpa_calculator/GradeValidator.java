62d7fc664cadd18a1c175acd354ab10510c0e6ea
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-03-26 14:23:08 -0400

    comments!

--- app/src/main/java/com/example/belenskid1_gpa_calculator/GradeValidator.java
+++ app/src/main/java/com/example/belenskid1_gpa_calculator/GradeValidator.java
@@ -2,19 +2,38 @@ package com.example.belenskid1_gpa_calculator;
 
 import android.widget.EditText;
 
+//class to validate a grade field
 public class GradeValidator {
+
+    //blank constructor
     public GradeValidator(){
 
     }
+
+    //validation method
+    //@param: EditText field to be evaluated
+    //@return: boolean value of content validity
     public Boolean validate(EditText field){
+
+        //getting text from object
         String gradeText = field.getText().toString();
+
+        //getting numeric data from text
         double grade = Double.parseDouble(gradeText);
+
+        //if there is no text
         if (gradeText.length() == 0)
             return false;
+
+        //if number entered is too large
         else if (grade > 100)
             return false;
+
+        //if number entered is too low
         else if (grade < 0)
             return false;
+
+        //input is valid
         else
             return true;
     }
--- app/src/main/java/com/example/belenskid1_gpa_calculator/MainActivity.java
+++ app/src/main/java/com/example/belenskid1_gpa_calculator/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //variable to remember if the screen fields are empty or not
     Boolean cleared = true;
 
 
@@ -22,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //check method runs validation on all fields on the screen before computing the GPA
     public void check(View view){
+
+        //validator to check the fields
         GradeValidator validator = new GradeValidator();
+
+        //creating an object for each field and button
         EditText course1 = (EditText) findViewById(R.id.course1EditText);
         EditText course2 = (EditText) findViewById(R.id.course2EditText);
         EditText course3 = (EditText) findViewById(R.id.course3EditText);
@@ -31,12 +37,18 @@ public class MainActivity extends AppCompatActivity {
         EditText course5 = (EditText) findViewById(R.id.course5EditText);
         Button clearAndCompute = (Button) findViewById(R.id.clearAndComputeButton);
         TextView gpa = (TextView) findViewById(R.id.GPATextView);
+
+        //booleans to store what fields are valid and which are not
         Boolean c1 = validator.validate(course1);
         Boolean c2 = validator.validate(course2);
         Boolean c3 = validator.validate(course3);
         Boolean c4 = validator.validate(course4);
         Boolean c5 = validator.validate(course5);
+
+        //if fields were empty to start with
         if (cleared){
+
+            //if all fields are currently valid and clear errors
             if (c1 && c2 && c3 && c4 &&c5){
                 course1.setError(null);
                 course2.setError(null);
@@ -47,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
                 compute(view);
                 cleared = !cleared;
             }
+
+            //else show what fields are no valid
             else{
                 if(!c1){
                     course1.setError("Please input a valid grade");
@@ -65,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+        //else clear all fields
         else{
             ConstraintLayout background = findViewById(R.id.background);
             course1.setText("");
@@ -80,7 +96,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //compute method to collect grade information and calculate the final average
     public void compute(View view){
+
+        //creating objects for relevant fields
         EditText course1 = (EditText) findViewById(R.id.course1EditText);
         EditText course2 = (EditText) findViewById(R.id.course2EditText);
         EditText course3 = (EditText) findViewById(R.id.course3EditText);
@@ -88,13 +107,19 @@ public class MainActivity extends AppCompatActivity {
         EditText course5 = (EditText) findViewById(R.id.course5EditText);
         TextView gpa = (TextView) findViewById(R.id.GPATextView);
         ConstraintLayout background = findViewById(R.id.background);
+
+        //getting the numeric grades from strings
         double grade1 = Double.parseDouble(course1.getText().toString());
         double grade2 = Double.parseDouble(course2.getText().toString());
         double grade3 = Double.parseDouble(course3.getText().toString());
         double grade4 = Double.parseDouble(course4.getText().toString());
         double grade5 = Double.parseDouble(course5.getText().toString());
+
+        //calculating GPA
         double average = (grade1 + grade2 + grade3 + grade4 + grade5) / 5.0;
         gpa.setText(Double.toString(average));
+
+        //evaluating response to the result
         if(average < 60)
             background.setBackgroundColor(Color.RED);
         else if(average < 80)
